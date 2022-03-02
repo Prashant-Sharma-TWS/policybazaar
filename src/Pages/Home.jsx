7674318ad7c92152fbf3e3172eac473fd1c796bf
Karthik <smkarthik98@gmail.com>
2022-03-03 00:39:29 +0530

    Completed PB Section Component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { OurPartners } from "../Components/HomeComponents/OurPartners";
+import { PBSection } from "../Components/HomeComponents/PBSection";
 import { SliderImage } from "../Components/HomeComponents/SliderImage";
 import { Top } from "../Components/HomeComponents/Top";
 
@@ -8,6 +9,8 @@ export const Home = () => {
     <div>
       <Top />
       <SliderImage />
+      <PBSection />
+
       <OurPartners />
     </div>
   );
