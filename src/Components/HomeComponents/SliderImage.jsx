caef2a27ca7284ce4c2fb860aa3d29e6dacdc92c
prashant-sharma-tws <ps142500@gmail.com>
2022-03-03 18:46:32 +0530

    checking

--- src/Components/HomeComponents/SliderImage.jsx
+++ src/Components/HomeComponents/SliderImage.jsx
@@ -1,44 +1,59 @@
 import React from "react";
-import { SlideOne } from "../../Elements/Home";
 import imageOne from "../../Images/slide-image-1.png";
 import imageTwo from "../../Images/slide-image-2.png";
 import imageThree from "../../Images/slide-image-3.png";
 import imageFour from "../../Images/slide-image-4.png";
+import styles from "./SecondSlider.module.css";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 export const SliderImage = () => {
-  const prev = () => {
-    const parent = document.getElementById("slide-img-one");
-    const childs = document.getElementsByClassName("slide-img-one-child");
-
-    parent.prepend(childs[childs.length - 1]);
+  const options = {
+    margin: 30,
+    responsiveClass: true,
+    nav: true,
+    autoplay: false,
+    navText: [`<span></span>`, `<span></span>`],
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   };
-  const next = () => {
-    const parent = document.getElementById("slide-img-one");
-    const childs = document.getElementsByClassName("slide-img-one-child");
-
-    parent.append(childs[0]);
-  };
-
   return (
-    <SlideOne>
-      <div id="slide-img-one">
-        <div className="slide-img-one-child">
-          <img src={imageOne} alt="slideimg1" />
-        </div>
-        <div className="slide-img-one-child">
-          <img src={imageTwo} alt="slideimg2" />
-        </div>
-        <div className="slide-img-one-child">
-          <img src={imageThree} alt="slideimg3" />
-        </div>
-        <div className="slide-img-one-child">
-          <img src={imageFour} alt="slideimg4" />
-        </div>
+    <div className={styles.slideContainer}>
+      {/* Starts */}
+      <div className={styles.slideWrapper}>
+        <OwlCarousel className={styles.sliderItems} {...options}>
+          <div className={styles.item}>
+            <img src={imageOne} alt="tax-img" className={styles.img1} />
+          </div>
+          <div className="item">
+            <img src={imageTwo} alt="tax-img" />
+          </div>
+          <div className="item">
+            <img src={imageThree} alt="tax-img" />
+          </div>
+          <div className="item">
+            <img src={imageFour} alt="tax-img" />
+          </div>
+        </OwlCarousel>
       </div>
-      <div>
-        <span onClick={prev}></span>
-        <span onClick={next}></span>
-      </div>
-    </SlideOne>
+      {/* Ends */}
+    </div>
   );
 };
